aaa172701e26e90a5ab55fcaad7719f0cfd2eaf1
kencrim <kencrim@gmail.com>
2025-02-20 10:01:43 -0800

    db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,14 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { sql, relations } from "drizzle-orm";
 import {
   index,
   integer,
   pgTableCreator,
   timestamp,
   varchar,
+  text,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -27,10 +28,61 @@ export const posts = createTable(
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
+
+export const workouts = createTable("workout", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  name: varchar("name", { length: 256 }),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+  description: text("description"),
+});
+
+export const workoutEntries = createTable("workout_entry", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  workoutId: integer("workout_id").references(() => workouts.id),
+  exerciseId: integer("exercise_id").references(() => exercises.id),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const exercises = createTable("exercise", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  name: varchar("name", { length: 256 }),
+  description: text("description"),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const workoutRelations = relations(workouts, ({ many }) => ({
+  entries: many(workoutEntries),
+}));
+
+export const workoutEntryRelations = relations(workoutEntries, ({ one }) => ({
+  workout: one(workouts, {
+    fields: [workoutEntries.workoutId],
+    references: [workouts.id],
+  }),
+  exercise: one(exercises, {
+    fields: [workoutEntries.exerciseId],
+    references: [exercises.id],
+  }),
+}));
