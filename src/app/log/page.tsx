1150c70ec8943d2e5081a7897857cfc7213ee11e
kencrim <kencrim@gmail.com>
2025-02-20 01:12:27 -0800

    are u there god it's me and i don't get neon

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,18 +1,17 @@
 import { WorkoutSummary } from "@/components/common/workout-summary";
 import { db } from "@/server/db";
 export default async function LogPage() {
-  // const posts = await db.query.posts.findMany();
+  const posts = await db.query.posts.findMany();
 
   return (
     <div>
       <WorkoutSummary />
 
-      {process.env.DATABASE_URL}
-      {/* {posts.map((post) => (
+      {posts.map((post) => (
         <div key={post.id}>
           <h2>{post.name}</h2>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
