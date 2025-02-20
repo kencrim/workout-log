96def379374fef9e53e4113dfff0e02390cf1337
kencrim <kencrim@gmail.com>
2025-02-20 01:02:00 -0800

    try tweaking env?

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,9 @@
 // src/db.ts
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
 import * as schema from "./schema";
-config({ path: ".env" }); // or .env.local
+import { env } from "@/env";
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(env.DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
