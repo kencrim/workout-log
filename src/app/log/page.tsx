ebdf5fef77df5d9d30030bd60ca3e2fb57e328ca
kencrim <kencrim@gmail.com>
2025-02-20 01:09:22 -0800

    pls

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,17 +1,18 @@
 import { WorkoutSummary } from "@/components/common/workout-summary";
 import { db } from "@/server/db";
 export default async function LogPage() {
-  const posts = await db.query.posts.findMany();
+  // const posts = await db.query.posts.findMany();
 
   return (
     <div>
       <WorkoutSummary />
 
-      {posts.map((post) => (
+      {process.env.DATABASE_URL}
+      {/* {posts.map((post) => (
         <div key={post.id}>
           <h2>{post.name}</h2>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,10 @@
 // src/db.ts
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 import * as schema from "./schema";
-import { env } from "@/env";
 
-const sql = neon(env.DATABASE_URL);
+config({ path: ".env" }); // or .env.local
 
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
