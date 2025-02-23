7a896fd819e3c9f1ebf374bf8afe3698749ba3d2
Ken Crimmins <37313064+kencrim@users.noreply.github.com>
2025-02-23 03:04:13 -0800

    remove signins from navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,21 +1,11 @@
 import Link from "next/link";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
   return (
     <nav className="flex w-full justify-between border-b p-4 font-semibold">
       <Link href="/">Workout Logger</Link>
       <div className="flex gap-4">
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton />
-        </SignedOut>
         <SignedIn>
           <UserButton />
         </SignedIn>
