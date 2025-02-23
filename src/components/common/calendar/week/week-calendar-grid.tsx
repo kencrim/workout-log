f0936b1a777913390b0f5af31318b2321f13a45c
kencrim <kencrim@gmail.com>
2025-02-22 17:18:04 -0800

    incremental work on calendar components

--- src/components/common/calendar/week/week-calendar-grid.tsx
+++ src/components/common/calendar/week/week-calendar-grid.tsx
@@ -1,3 +1,32 @@
+const TIMES = [
+  "12AM",
+  "1AM",
+  "2AM",
+  "3AM",
+  "4AM",
+  "5AM",
+  "6AM",
+  "7AM",
+  "8AM",
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+  "6PM",
+  "7PM",
+  "8PM",
+  "9PM",
+  "10PM",
+  "11PM",
+];
+
+import React from "react";
+
 export function WeekCalendarGrid({
   containerOffset,
 }: {
@@ -8,155 +37,20 @@ export function WeekCalendarGrid({
       {/* Horizontal lines */}
       <div
         className="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"
-        style={{ gridTemplateRows: "repeat(48, minmax(3.5rem, 1fr))" }}
+        style={{ gridTemplateRows: "repeat(48, minmax(2.5rem, 1fr))" }}
       >
         <div ref={containerOffset} className="row-end-1 h-7"></div>
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            12AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            1AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            2AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            3AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            4AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            5AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            6AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            7AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            8AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            9AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            10AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            11AM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            12PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            1PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            2PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            3PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            4PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            5PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            6PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            7PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            8PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            9PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            10PM
-          </div>
-        </div>
-        <div />
-        <div>
-          <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
-            11PM
-          </div>
-        </div>
-        <div />
+        {TIMES.map((t) => (
+          <React.Fragment key={t}>
+            <div>
+              <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs/5 text-gray-400">
+                {t}
+              </div>
+            </div>
+            <div />
+          </React.Fragment>
+        ))}
       </div>
-
       {/* Vertical lines */}
       <div className="col-start-1 col-end-2 row-start-1 hidden grid-cols-7 grid-rows-1 divide-x divide-gray-100 sm:grid sm:grid-cols-7">
         <div className="col-start-1 row-span-full" />
